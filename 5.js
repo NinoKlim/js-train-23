cd9890006c80d7716850c27dc07be7ad82312724
NinoKlim <nnikitka@gmail.com>
2024-09-24 15:03:33 +0200

    js train 23

--- 5.js
+++ 5.js
@@ -4,10 +4,18 @@
 // Клас BankTransfer представляє собою систему для здійснення банківських переказів
 class BankTransfer {
   // Зробіть метод initiateTransfer, який приймає amount та відповідає за ініціювання банківського переказу
-  // Він приймає суму переказу як параметр
-  // Для ініціювання банківського переказу спершу обчислюється сума з урахуванням комісії calculatedAmount = this.calculateFee(amount)
-  // Виводимо інформацію про ініціювання банківського переказу Ініціюємо банківський переказ: $${calculatedAmount}
+  initiateTransfer(amount) {
+    // Він приймає суму переказу як параметр
+    // Для ініціювання банківського переказу спершу обчислюється сума з урахуванням комісії calculatedAmount = this.calculateFee(amount)
+    // Виводимо інформацію про ініціювання банківського переказу Ініціюємо банківський переказ: $${calculatedAmount}
+    const calculatedAmount = this.calculateFee(amount);
+    console.log(`Initiate bank transfer: $${calculatedAmount}`);
+  }
   // Зробіть метод calculateFee, який відповідає за розрахунок комісії за переказ
+  calculateFee(amount) {
+    const totalAmount = amount * 1.02;
+    return totalAmount;
+  }
   // Він приймає amount переказу як параметр та повертає число після розрахування комісії
   // Логіка розрахунку комісії за переказ amount * 1.02
   // Припустимо, комісія становить 2% від суми переказу
@@ -16,6 +24,9 @@ class BankTransfer {
 // Клас WalletTransfer представляє собою систему для здійснення переказів з гаманця
 class WalletTransfer {
   // Створіть метод processTransfer, який відповідає за здійснення переказу з гаманця
+  proccesTransfer(amount) {
+    console.log(`Transfering from wallet: $${amount}`);
+  }
   // Він приймає суму переказу як параметр
   // Виводимо інформацію про здійснення переказу з гаманця Здійснюємо переказ з гаманця: $${amount}
 }
@@ -25,20 +36,46 @@ class WalletTransfer {
 class TransferAdapter {
   // Робимо конструктор, що приймає об'єкт transferSystem типу WalletTransfer
   // Зберігаємо посилання на об'єкт WalletTransfer у властивості transferSystem
+  constructor(transferSystem) {
+    this.transferSystem = transferSystem;
+  }
   // Робимо метод initiateTransfer, який адаптує API WalletTransfer до API BankTransfer.
-  // Він приймає amount як аргумент та повертає результат виконання переказу.
-  // Викликаємо допоміжний метод calculateFee для обчислення комісії за переказ та результат записуєм в константу calculatedAmount
-  // Викликаємо метод processTransfer об'єкту WalletTransfer з calculatedAmount.
-  // В результаті повертаємо результат виконання переказу.
+  initiateTransfer(amount) {
+    // Він приймає amount як аргумент та повертає результат виконання переказу.
+    // Викликаємо допоміжний метод calculateFee для обчислення комісії за переказ та результат записуєм в константу calculatedAmount
+    // Викликаємо метод processTransfer об'єкту WalletTransfer з calculatedAmount.
+    // В результаті повертаємо результат виконання переказу.
+    const calculatedAmount = this.calculateFee(amount);
+    return this.transferSystem.proccesTransfer(calculatedAmount);
+  }
+  calculateFee(amount) {
+    return amount * 1.2;
+  }
   // Створюємо метод calculateFee, що приймає amount та обчислює суму комісії за переказ amount * 1.2, засновуючись на вхідній сумі.
   // Повертаємо amount * 1.2
+}
+
+class Order {
+  constructor(amount) {
+    this.amount = amount;
+
+    if (amount < 100) {
+      this.paymentSystem = new TransferAdapter(new WalletTransfer());
+    } else {
+      this.paymentSystem = new BankTransfer();
+    }
+  }
+
+  initiateTransfer() {
+    return this.paymentSystem.initiateTransfer(this.amount);
+  }
 }
 console.log("Завдання 5 ====================================");
 // Після виконання розкоментуйте код нижче
 
 // Створимо екземпляри BankTransfer
-// const purchase1 = new BankTransfer();
-// purchase1.initiateTransfer(1000);
+const purchase1 = new Order(1000);
+purchase1.initiateTransfer();
 
-// const purchase2 = new BankTransfer();
-// purchase2.initiateTransfer(10);
+const purchase2 = new Order(10);
+purchase2.initiateTransfer();
